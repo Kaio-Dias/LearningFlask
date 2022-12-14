776691cec88bbfa766bc8e8749612bf6a6976dc0
9Cloud6 <86304172+9Cloud6@users.noreply.github.com>
2022-12-14 18:20:40 -0300

    o HTML embutido no content valor não é renderizado como HTML porque o mecanismo de modelagem (Jinja) escapa automaticamente do conteúdo HTML.

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -1,7 +1,17 @@
-from flask import Flask
-from HelloFlask import app 
+from datetime import datetime
+from flask import render_template
+from HelloFlask import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello World"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = " on " + formatted_now)
+
+
